4a92c230e4b2aece58798671c56a0af2bf580e47
Siyovush <paraparadox@protonmail.com>
2023-04-10 19:12:42 +0500

    WIP: Structure & Relations

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"fmt"
+	"github.com/softclub-go-0-0/blog-service/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+const (
+	host     = "localhost" // 127.0.0.1
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "blog_service"
+)
+
+func main() {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Dushanbe", host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("successfully connected")
+
+	db.AutoMigrate(&models.User{}, &models.Post{}, &models.Comment{})
+
+	user := models.User{}
+	//
+	//db.Create(&user)
+
+	db.First(&user, 1)
+
+	fmt.Println("Selected user:", user)
+
+	post := models.Post{
+		UserID:  user.ID,
+		Title:   "SOME TEXT",
+		Content: "Dubai",
+	}
+
+	db.Create(&post)
+
+	fmt.Println("Created post:", post)
+
+	comment := models.Comment{
+		PostID: post.ID.ID,
+		UserID: user.ID,
+		Text:   "Some comment",
+	}
+
+	db.Create(&comment)
+
+	fmt.Println("Created comment:", comment)
+
+}
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,14 +1,48 @@
 package models
 
-import "database/sql"
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type ID struct {
+	ID uint `gorm:"primarykey"`
+}
+
+type Timestamps struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
 
 // ORM - Object-Relational Mapping
 
 type User struct {
-	ID        uint
+	ID        uint `gorm:"primarykey"`
 	Name      string
 	Email     string
-	CreatedAt sql.NullTime
-	UpdatedAt sql.NullTime
-	DeletedAt sql.NullTime
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	Posts    []Post
+	Comments []Comment
+}
+
+type Post struct {
+	ID
+	UserID  uint
+	Cover   string
+	Title   string
+	Content string
+	Timestamps
+
+	Comments []Comment
+}
+
+type Comment struct {
+	gorm.Model
+	PostID uint
+	UserID uint
+	Text   string
 }
